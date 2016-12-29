326052539ec32d04e0ca3f25eaf2c2b7757796f9
Luiz Gustavo Costa <luizgustavo@luizgustavo.pro.br>
2016-12-29 11:05:17 -0200

    changes to new squid package

--- pf2ad.sh
+++ pf2ad.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-VERSION='20161118001'
+VERSION='20161229005' # Happy new year 2017 !
 
 if [ -f "/etc/samba.patch.version" ]; then
 	if [ "$(cat /etc/samba.patch.version)" = "$VERSION" ]; then
